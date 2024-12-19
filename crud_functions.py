d5e051ec242b193abbab695952f8fd99c55b55ac
LoLawlietED <stivsasha@gmail.com>
2024-12-19 15:15:18 +0700

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+def initiate_db():
+    connection = sqlite3.connect("products.db")
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS Users;")
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        price INTEGER NOT NULL
+    )
+    ''')
+    connection.commit()
+    connection.close()
+def get_all_products():
+    connection = sqlite3.connect("products.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM Products")
+    products = cursor.fetchall()
+
+    connection.close()
+    return products
